a073b4d8eee2c6652258e72b2e621e0322d48138
Iqra Arif <bintearif79@gmail.com>
2025-05-17 23:44:36 -0700

    Add files via upload

---
+++ Data types assignment.py
@@ -0,0 +1,28 @@
+# #Task 1:Integer
+# age = 21
+# print("In five years, you will be", age +5)
+
+# #Task 2: String 
+# name = "Iqra"
+# greeting = "Hello " + name
+# print(greeting)
+
+# #Task 3: Float
+# heigth = 2.60
+# print("Your heigth is", heigth, "meters.")
+
+# #Task 4: Combine use
+# item = "Book"
+# quantity = 4
+# price_per_item = 12.99
+# total_cost = quantity * price_per_item
+# print("You bougth",quantity, item + "s", "for a total of", total_cost,"dollars.")
+
+#Task 5: Challenge myself!
+#We take inputs
+name = input("Enter your name:")
+item = input("Enter the item:")
+price = float(input("Enter the price per item:"))
+quantity = int(input("Enter the quantity:"))
+total_price = price * quantity
+print("Hello" , name  ," ! you bought" , quantity, item + "s" , ". Totl:" , total_price , "dollars.")
